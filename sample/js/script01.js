c875659e9d5cdeb2c63d0a333b52acab517e6b70
sakurai-coma <s-sakurai0271@st.kyoto-art.ac.jp>
2021-10-08 15:25:52 +0900

    dekiru

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -3,4 +3,18 @@ $(funtion(){
     $("#button01").on("click",funtion(){
         $(".back-red").hide();
     })
-    })
+    
+
+$("#button02").on("click",funtion(){
+    $(".back-blue").hide();
+})
+
+$("#button03").on("click",funtion(){
+    $("#box03,#box07,#box08").hide();
+})
+
+$("#button04").on("click",funtion({
+    $(".box").show();
+})
+
+})
